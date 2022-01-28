dc61c3842e617b7f1226c06114e11da76b00da37
lsunko <sunko.elena2001@gmail.com>
2022-01-28 17:07:53 +0300

    Add future project structure

---
+++ src/main/java/ru/hse/software/design/CLI.java
@@ -0,0 +1,7 @@
+package ru.hse.software.design;
+
+public class CLI {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/test/java/ru/hse/software/design/CLITests.java
@@ -0,0 +1,11 @@
+package ru.hse.software.design;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CLITests {
+    @Test
+    public void testCLI() {
+        Assertions.assertTrue(true);
+    }
+}
