347bf674c4a5f4c8f46ac7170e5965106ce6ea57
lizazhemchuzhina <lizazhemchuzhina@mail.ru>
2022-01-29 20:21:56 +0300

    add command builder

--- src/main/java/ru/hse/software/design/Parser.java
+++ src/main/java/ru/hse/software/design/Parser.java
@@ -7,6 +7,7 @@ public class Parser {
     public static CommandTokens preProcess(List<Token> tokens) {
         String command = tokens.get(0).getToken();
         List<String> commandArgs = new ArrayList<>();
+
         for (int i = 1; i < tokens.size(); i++) {
             commandArgs.add(tokens.get(i).getToken());
         }
