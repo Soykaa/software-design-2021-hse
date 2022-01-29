f72658c648be7a6632520e2d3d9e5726421680dd
lizazhemchuzhina <lizazhemchuzhina@mail.ru>
2022-01-29 20:34:12 +0300

    tried to complete parser

--- src/main/java/ru/hse/software/design/Parser.java
+++ src/main/java/ru/hse/software/design/Parser.java
@@ -7,7 +7,18 @@ public class Parser {
     public static CommandTokens preProcess(List<Token> tokens) {
         String command = tokens.get(0).getToken();
         List<String> commandArgs = new ArrayList<>();
-
+        if (command.contains("=")) {
+            boolean insideSingleQuotes = false;
+            boolean insideDoubleQuotes = false;
+            String variable = "";
+            String value = "";
+            for (Character symbol : command.toCharArray()) {
+                if (symbol == '\''){
+                    insideSingleQuotes = !insideSingleQuotes;
+                }
+                // need to do something with this 
+            }
+        }
         for (int i = 1; i < tokens.size(); i++) {
             commandArgs.add(tokens.get(i).getToken());
         }
