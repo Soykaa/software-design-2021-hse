88c66ac7d139a617cbbab8de5a8ca39526f8705c
Kate <ekaterinaicenko@gmail.com>
2022-02-04 17:34:42 +0300

    Fix formatting

--- src/test/java/ru/hse/software/design/commands/EchoCommandTests.java
+++ src/test/java/ru/hse/software/design/commands/EchoCommandTests.java
@@ -67,6 +67,4 @@ public class EchoCommandTests {
             e.printStackTrace();
         }
     }
-
-
 }
--- src/test/java/ru/hse/software/design/commands/EnvironmentCommandTest.java
+++ src/test/java/ru/hse/software/design/commands/EnvironmentCommandTest.java
@@ -81,6 +81,4 @@ public class EnvironmentCommandTest {
             e.printStackTrace();
         }
     }
-
-
 }
