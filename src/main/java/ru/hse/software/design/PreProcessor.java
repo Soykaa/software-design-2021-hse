6a26e4544189d8f52f00b761ab2e968df3ad6fde
Kate <ekaterinaicenko@gmail.com>
2022-02-06 00:20:03 +0300

    Finish PreProcessor and add some tests for it

--- src/main/java/ru/hse/software/design/PreProcessor.java
+++ src/main/java/ru/hse/software/design/PreProcessor.java
@@ -2,15 +2,30 @@ package ru.hse.software.design;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class PreProcessor {
-    private Token processToken(Token token) {
+    private static String getFromEnvironment(String variableName) {
+        Optional<String> value = Environment.get(variableName);
+        if (value.isEmpty()) {
+            throw new NoSuchElementException("No such variable");
+        }
+        return value.get();
+    }
+
+    private static Token processToken(Token token) {
         StringBuilder processedToken = new StringBuilder();
         StringBuilder variableName = new StringBuilder();
         boolean nameStarted = false;
         for (int i = 0; i < token.getToken().length(); i++) {
             switch (token.getToken().charAt(i)) {
                 case '$':
+                    if (nameStarted) {
+                        String value = getFromEnvironment(variableName.toString());
+                        processedToken.append(value);
+                        variableName.setLength(0);
+                    }
                     nameStarted = true;
                     break;
                 case '|':
@@ -18,8 +33,10 @@ public class PreProcessor {
                 case ' ':
                 case '\'':
                     if (nameStarted) {
-                        // Env call
-                        // end of current name
+                        String value = getFromEnvironment(variableName.toString());
+                        processedToken.append(value);
+                        nameStarted = false;
+                        variableName.setLength(0);
                     }
                     processedToken.append(token.getToken().charAt(i));
                     break;
@@ -32,6 +49,9 @@ public class PreProcessor {
                     break;
             }
         }
+        if (nameStarted) {
+            processedToken.append(getFromEnvironment(variableName.toString()));
+        }
         token.setToken(processedToken.toString());
         token.setType(Type.FULLY_PROCESSED);
         return token;
@@ -43,7 +63,7 @@ public class PreProcessor {
             if (token.getType() == Type.FULLY_PROCESSED) {
                 result.add(token);
             } else {
-                throw new UnsupportedOperationException();
+                result.add(processToken(token));
             }
         }
         return result;
