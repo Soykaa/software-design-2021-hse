436d80fba34338054a254c4df8dac8db1a0825ab
kirill <turovkv@yandex.ru>
2022-03-04 22:22:52 +0300

    Add LsCommand

---
+++ src/main/java/ru/hse/software/design/commands/LsCommand.java
@@ -0,0 +1,50 @@
+package ru.hse.software.design.commands;
+
+import ru.hse.software.design.Environment;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Prints contents of the directory
+ **/
+public class LsCommand extends Command {
+    private final List<String> commandArgs = new ArrayList<>();
+
+    /**
+     * Created ls command with given arguments.
+     *
+     * @param commandArgs command arguments
+     **/
+    public LsCommand(List<String> commandArgs) {
+        this.commandArgs.addAll(commandArgs);
+        this.command = "ls";
+    }
+
+    /**
+     * Executes 'ls' command with the given arguments.
+     *
+     * @return 0
+     **/
+    @Override
+    public int execute(String input) {
+        Path path;
+        if (commandArgs.isEmpty()) {
+            path = Environment.getCurrentFolderPath();
+        } else {
+            path = Environment.getRelativePath(commandArgs.get(0));
+        }
+        File fileName = new File(String.valueOf(path));
+        File[] fileList = fileName.listFiles();
+
+        StringBuilder sb = new StringBuilder();
+        assert fileList != null;
+        for (File file : fileList) {
+            sb.append(file.getName()).append("\n");
+        }
+        output = sb.toString();
+        return 0;
+    }
+}
